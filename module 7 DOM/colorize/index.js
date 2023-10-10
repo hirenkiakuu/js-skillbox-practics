85fa2b25bb09e9e230d73ad1a331a8368bf060b8
Vladislav <vladdik9@gmail.com>
2023-10-10 23:21:57 +0500

    deleted trash

--- module 7 DOM/colorize/index.js
+++ module 7 DOM/colorize/index.js
@@ -20,37 +20,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
-
-
-
-
-
-
-// let h1 = document.createElement('h1');
-// h1.textContent = 'Список покупок';
-// document.body.append(h1);
-
-// let ol = document.createElement('ol');
-// document.body.append(ol);
-
-// let list = [
-//     document.createElement('li'),
-//     document.createElement('li'),
-//     document.createElement('li')
-// ];
-
-// list[0].textContent = 'бензопила';
-// list[1].textContent  = 'молоко';
-// list[2].textContent  = 'хлеб';
-
-// ol.prepend(list[0]);
-// ol.prepend(list[1]);
-// ol.prepend(list[2]);
-// let eggs = document.createElement('li');
-// eggs.textContent = 'яйки';
-// list[2].before(eggs);
-
-// let sausage = document.createElement('li');
-// sausage.textContent = 'sosisk';
-// list[0].after(sausage);
-
